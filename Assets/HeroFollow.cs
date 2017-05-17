6090f7f3d676543be2ffe94c46ea11244c3a6832
yaroslav <gavryshko.sl@gmail.com>
2017-05-17 17:37:03 +0300

    Improved movements,camera follows hero

---
+++ Assets/HeroFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+	public HeroRabit rabit;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		//Get access to component Transform
+		//GetComponent<Transform>
+		Transform rabit_transform = rabit.transform;
+		//Get access to component Transform camera
+		Transform camera_transform = this.transform;
+		//Get access to rabbit's coordinates
+		Vector3 rabit_position = rabit_transform.position;
+		Vector3 camera_position = camera_transform.position;
+		//Move camera only by X,Y
+		camera_position.x = rabit_position.x;
+		camera_position.y = rabit_position.y;
+		//Set coordinates of camera
+		camera_transform.position = camera_position;
+	}
+}
